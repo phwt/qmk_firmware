0c5f27c1f6ca2a73546927489b28a96edd9d6e30
phwt.smwt <phwt.smwt@gmail.com>
2022-04-11 01:19:58 +0700

    system sleep hotkey

--- keyboards/idobao/id75/keymaps/phwt/keymap.c
+++ keyboards/idobao/id75/keymaps/phwt/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // clang-format o
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_P7,   KC_P8,   KC_P9,   KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
         KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_P1,   KC_P2,   KC_P3,   KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_SPC,   MO(_FN), KC_PPLS, KC_P0,   KC_PDOT, MO(_FN), KC_SPC,  RAISE,   _______, _______, KC_RALT
+        KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_SPC,   MO(_FN), KC_PPLS, KC_P0,   KC_PDOT, MO(_FN), KC_SPC,  RAISE,   _______, _______, KC_RSFT
     ),
     [_QTY] = LAYOUT_ortho_5x15(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -165,10 +165,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 };
 
-const key_override_t ctrl_esc_term = ko_make_basic(MOD_MASK_CTRL, KC_ESC, C(KC_GRV));
+const key_override_t ctrl_esc_term         = ko_make_basic(MOD_MASK_CTRL, KC_ESC, C(KC_GRV));
+const key_override_t shift_backspace_sleep = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_SLEP);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
-    &ctrl_esc_term,
+    &ctrl_esc_term, &shift_backspace_sleep,
     NULL // Null terminate the array of overrides!
 };
