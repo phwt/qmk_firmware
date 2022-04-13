ba36575ee720e432e3e60d1fdd6f713b3c266554
phwt.smwt <phwt.smwt@gmail.com>
2022-04-13 22:30:17 +0700

    feat: move KC_SAPS to right side

--- keyboards/idobao/id75/keymaps/phwt/keymap.c
+++ keyboards/idobao/id75/keymaps/phwt/keymap.c
@@ -35,13 +35,13 @@ enum custom_keycodes {
     TOGGLE
 };
 
-enum { TD_LSFT_CAPS = 0 };
+enum { TD_RSFT_CAPS = 0 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS) //
+    [TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS) //
 };
 
-#define KC_SAPS TD(TD_LSFT_CAPS)
+#define KC_SAPS TD(TD_RSFT_CAPS)
 
 enum layers {
     _CMK, // Colemak-DH Matrix
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // clang-format o
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PEQL, KC_PAST, KC_PMNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_P7,   KC_P8,   KC_P9,   KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
         KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
-        KC_SAPS, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_P1,   KC_P2,   KC_P3,   KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_P1,   KC_P2,   KC_P3,   KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_UP,   KC_SAPS,
         KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  KC_SPC,  KC_P0,   KC_PPLS, MO(_FN), RAISE,   KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_QTY] = LAYOUT_ortho_5x15(
