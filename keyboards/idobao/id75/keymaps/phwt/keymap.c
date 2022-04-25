d7962835e16647517da62484d43546512d3eee4b
phwt.smwt <phwt.smwt@gmail.com>
2022-04-25 21:24:22 +0700

    default arrow cluster

--- keyboards/idobao/id75/keymaps/phwt/keymap.c
+++ keyboards/idobao/id75/keymaps/phwt/keymap.c
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // clang-format o
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
         KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        KC_LCTL, MO(_FN), KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_SPC,  KC_P0,   TOGGLE, LOWER,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
+        KC_LCTL, MO(_FN), KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_SPC,  KC_P0,   TOGGLE, LOWER,   RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_COLEMAK] = LAYOUT_ortho_5x15(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // clang-format o
 const uint16_t PROGMEM _reset[]        = {LOWER, RAISE, KC_ESC, COMBO_END};
 const uint16_t PROGMEM _ltoggle[]      = {LOWER, RAISE, COMBO_END};
 const uint16_t PROGMEM _arrowtoggle[]  = {KC_LBRC, KC_RBRC, KC_MINS, KC_EQL, COMBO_END};
-const uint16_t PROGMEM _symboltoggle[] = {KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, COMBO_END};
+const uint16_t PROGMEM _symboltoggle[] = {KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(_reset, RESET),               //
