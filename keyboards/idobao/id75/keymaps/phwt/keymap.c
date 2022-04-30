81708ff97183c91127d0bd3aee2aa00c27318574
phwt.smwt <phwt.smwt@gmail.com>
2022-04-30 14:47:44 +0700

    update layout

--- keyboards/idobao/id75/keymaps/phwt/keymap.c
+++ keyboards/idobao/id75/keymaps/phwt/keymap.c
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
     [_QWERTY] = LAYOUT_ortho_5x15(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TH_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
         TH_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,   KC_N,    KC_M,    KC_COMM, KC_DOT,  TH_UP,   KC_SLSH,
-        KC_LCTL, MO(_FN), KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_SPC,  KC_P0,   TOGGLE,  LOWER,   RAISE,   KC_RALT, TH_LEFT, TH_DOWN, TH_RGHT
+        KC_LCTL, MO(_FN), KC_LALT, KC_LGUI, KC_SPC,  LOWER,   KC_SPC,  KC_P0,   TOGGLE,  RAISE,   KC_RSFT, KC_RALT, TH_LEFT, TH_DOWN, TH_RGHT
     ),
     [_COLEMAK] = LAYOUT_ortho_5x15(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT_ortho_5x15(
         KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_DEL,
         KC_TILD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_CAPS, KC_CIRC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DLR,  KC_PIPE,
+        KC_CAPS, KC_CIRC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DLR,  KC_QUOT,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, ADJUST,  _______, _______, _______, _______, _______
     ),
