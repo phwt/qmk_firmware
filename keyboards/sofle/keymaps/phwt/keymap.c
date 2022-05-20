eb5a2cbbe4c124e765d9b2b9723054293ac0745b
phwt.smwt <phwt.smwt@gmail.com>
2022-05-20 18:46:06 +0700

    feat: update sofle keymap

--- keyboards/sofle/keymaps/phwt/keymap.c
+++ keyboards/sofle/keymaps/phwt/keymap.c
@@ -27,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TH_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-  TH_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  TH_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  LOWER,        RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+                    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  LOWER,        RAISE, KC_ENT,  KC_RGUI, KC_LBRC, KC_RBRC
 ),
 [_COLEMAK] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
@@ -40,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_LOWER] = LAYOUT(
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, _______, KC_UP,   _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-  KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                      _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+  KC_CAPS, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
                     _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
 ),
 [_RAISE] = LAYOUT(
   KC_GRV,  _______, _______, _______, _______, _______,                      _______, _______, _______, KC_MINS, KC_EQL,  KC_DEL,
-  KC_TILD, _______, _______, _______, _______, _______,                      _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS,
-  KC_CAPS, KC_CIRC, _______, _______, C(KC_F), _______,                      _______, C(KC_J), _______, _______, KC_DLR,  KC_QUOT,
+  KC_TILD, _______, _______, _______, _______, _______,                      _______, _______, KC_UP,   KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_CAPS, KC_CIRC, _______, _______, C(KC_F), C(KC_J),                      _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,  KC_QUOT,
   _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
                     _______, _______, _______, _______, ADJUST,     _______, _______, _______, _______, _______
 ),
