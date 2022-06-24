af96f686997a9a231d58c4af7d00059e6e5f62c3
phwt.smwt <phwt.smwt@gmail.com>
2022-06-25 00:50:32 +0700

    update layout

--- keyboards/sofle/keymaps/phwt/keymap.c
+++ keyboards/sofle/keymaps/phwt/keymap.c
@@ -6,6 +6,7 @@ enum sofle_layers {
     _WINDOWS,
     _LOWER,
     _RAISE,
+    _HIGHER,
     _ADJUST,
 };
 
@@ -17,6 +18,7 @@ enum custom_keycodes {
 
 #define LOWER OSL(_LOWER)
 #define RAISE OSL(_RAISE)
+#define HIGHER OSL(_HIGHER)
 #define ADJUST MO(_ADJUST)
 
 #define OS_LSFT OSM(MOD_LSFT)
@@ -37,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
         TH_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          OS_LCTL, OS_LALT, KC_LGUI, KC_SPC,  LOWER,        RAISE, KC_ENT,  KC_RGUI, KC_LBRC, KC_RBRC
+                          KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  LOWER,        RAISE, KC_ENT,  KC_RGUI, KC_LBRC, KC_RBRC
     ),
     [_COLEMAK] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
@@ -51,21 +53,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-                          _______, KC_LGUI, OS_LALT, _______, _______,    _______, _______, _______, _______, _______
+                          _______, KC_LGUI, KC_LALT, _______, _______,    _______, _______, _______, _______, _______
     ),
     [_LOWER] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12,
-        KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-                          _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
+        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_BSLS, KC_F12,
+        _______, _______, _______, _______, KC_PLUS, KC_MINS,                      KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, _______, _______,
+        _______, _______, _______, _______, _______, KC_EQL,  _______,    _______, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   _______, _______,
+                          _______, _______, _______, KC_UNDS, _______,     HIGHER, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT(
-        KC_GRV,  _______, _______, _______, _______, _______,                      _______, _______, _______, KC_MINS, KC_EQL,  KC_DEL,
+        KC_GRV,  _______, _______, _______, _______, _______,                      _______, _______, _______, KC_MINS, KC_EQL,  _______,
         KC_TILD, _______, _______, _______, _______, _______,                      _______, _______, KC_UP,   KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_CAPS, KC_CIRC, _______, _______, C(KC_F), C(KC_J),                      _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,  KC_QUOT,
+        _______, KC_CIRC, _______, _______, C(KC_F), C(KC_J),                      _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,  KC_PIPE,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
                           _______, _______, _______, _______, ADJUST,     _______, _______, _______, _______, _______
+    ),
+    [_HIGHER] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+        _______, KC_PPLS, KC_P7,   KC_P8,   KC_P9,   KC_PAST,                      _______, KC_HOME, KC_UP,   KC_END,  _______, _______,
+        _______, KC_PMNS, KC_P4,   KC_P5,   KC_P6,   KC_PSLS,                      _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+        _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PEQL, _______,    _______, _______, _______, _______, _______, _______, _______,
+                          _______, _______, _______, _______, XXXXXXX,    XXXXXXX, _______, _______, _______, _______
     ),
     [_ADJUST] = LAYOUT(
         RESET,   _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
@@ -177,21 +186,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case TOGOS:
             if (record->event.pressed) layer_invert(_WINDOWS);
             return false;
-        case KC_ENT: // Enter as space when modifier is pressed
-            if (record->event.pressed && !record->tap.count) {
-                if (keyboard_report->mods) {
-                    if (get_mods() & MOD_MASK_GUI)
-                        register_code(KC_LGUI);
-                    else if (get_mods() & MOD_MASK_ALT)
-                        register_code(KC_LALT);
+        // case KC_ENT: // Enter as space when modifier is pressed
+        //     if (record->event.pressed && !record->tap.count) {
+        //         if (keyboard_report->mods) {
+        //             if (get_mods() & MOD_MASK_GUI)
+        //                 register_code(KC_LGUI);
+        //             else if (get_mods() & MOD_MASK_ALT)
+        //                 register_code(KC_LALT);
 
-                    wait_ms(100);
-                    tap_code16(KC_SPC);
-                    clear_keyboard();
-                    return false;
-                }
-            }
-            return true;
+        //             wait_ms(100);
+        //             tap_code16(KC_SPC);
+        //             clear_keyboard();
+        //             return false;
+        //         }
+        //     }
+        //     return true;
         case TH_BSPC:
             if (record->event.pressed && !record->tap.count) {
                 tap_code16(A(KC_BSPC));
