5d39b0d1b05d94ac8cf1391850c2c3433244bf00
phwt <28344318+phwt@users.noreply.github.com>
2024-10-28 23:08:45 +0700

    feat(planck): update keymap

--- keyboards/planck/keymaps/phwt/keymap.c
+++ keyboards/planck/keymaps/phwt/keymap.c
@@ -16,13 +16,18 @@
 
 #include QMK_KEYBOARD_H
 
-enum layers { _BASE, _LOWER, _RAISE, _NUM, _ANSI };
-enum keycodes { M_BHREG = SAFE_RANGE };
+enum layers { _BASE, _ANSI, _WIN, _LOWER, _RAISE, _ADJUST, _NUM };
+enum keycodes {
+    M_BHREG = SAFE_RANGE, // Vim black hole register
+    M_LSNBC               // Vim go to last non-blank character
+};
 
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
+#define ADJUST MO(_ADJUST)
 #define NUM    MO(_NUM)
 #define ANSI   TG(_ANSI)
+#define WIN    TG(_WIN)
 #define TH_ENT LT(_RAISE, KC_ENT)
 
 /* clang-format off */
@@ -31,19 +36,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, M_BHREG, KC_LALT, KC_LGUI, KC_SPC,  LOWER,   LOWER,   TH_ENT,  KC_RGUI, NUM,     KC_HOME, KC_END
+        KC_LCTL, M_BHREG, KC_LALT, KC_LGUI, KC_SPC,  LOWER,   LOWER,   TH_ENT,  KC_RGUI, M_LSNBC, KC_HOME, KC_END
     ),
     [_LOWER] = LAYOUT_planck_grid(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        _______, _______, _______, _______, KC_PLUS, _______, _______, KC_UNDS, _______, _______, KC_LBRC, KC_RBRC,
-        _______, KC_PIPE, KC_BSLS, _______, KC_MINS, _______, _______, KC_EQL,  _______, _______, KC_MINS, KC_EQL,
-        _______, _______, _______, _______, _______, _______, _______, ANSI,    _______, _______, _______, _______
+        _______, _______, _______, _______, KC_PLUS, KC_PIPE, KC_QUES, KC_UNDS, _______, _______, KC_LBRC, KC_RBRC,
+        _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MINS, KC_BSLS, KC_SLSH, KC_EQL,  _______, _______, KC_MINS, KC_EQL,
+        _______, _______, _______, _______, _______, _______, _______, ADJUST, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT_planck_grid(
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, _______, _______, _______, KC_WH_D, _______, _______, KC_LEFT, KC_UP,   KC_RGHT, _______, _______,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_WH_U, _______, _______, _______, KC_DOWN, _______, _______, _______,
+        _______, _______, KC_MS_L, KC_MS_U, KC_MS_R, KC_WH_D, _______, KC_LEFT, KC_UP,   KC_RGHT, _______, _______,
+        _______, _______, KC_BTN1, KC_MS_D, KC_BTN2, KC_WH_U, _______, KC_HOME, KC_DOWN, KC_END,  _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_ADJUST] = LAYOUT_planck_grid(
+        QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        UG_TOGG, AU_TOGG, _______, _______, _______, _______, _______, _______, WIN,     ANSI,    _______, _______
     ),
     [_NUM] = LAYOUT_planck_grid(
         _______, KC_P7,   KC_P8,   KC_P9,   _______, _______, _______, _______, _______, _______, _______, _______,
@@ -56,7 +67,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_B,    KC_Z,    KC_X,    KC_C,    KC_V,    _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    )
+    ),
+    [_WIN] = LAYOUT_planck_grid(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
 };
 /* clang-format on */
 
@@ -65,6 +82,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case M_BHREG:
             if (record->event.pressed) {
                 send_string("\"_");
+            }
+            return false;
+        case M_LSNBC:
+            if (record->event.pressed) {
+                send_string("g_");
             }
             return false;
     }
